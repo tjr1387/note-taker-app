3d4cccb4a747dfe1c94d2fb4ec69c2b092cee72c
Hair_Trigg3r <tylerjreimer@gmail.com>
2022-12-12 16:48:28 -0500

    Add DELETE route for /api/notes

--- routes/notesRouter.js
+++ routes/notesRouter.js
@@ -2,14 +2,25 @@ const router = require('express').Router();
 const uniqid = require('uniqid');
 const { readFromFile, readAndAppend, writeToFile } = require('../helpers/fsUtils');
 
+
 // GET route for the full JSON 'db' file
 router.get('/', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
-
 // DELETE route for one note (by ID)
-
+router.delete('/:note_id', (req, res) => {
+    const noteId = req.params.note_id;
+    readFromFile('./db/db.json')
+        .then((data) => JSON.parse(data))
+        .then((json) => {
+            // The array in db.json without the _one_ that is to be deleted, then overwrite db.json with this new array
+            const dbAfterDelete = json.filter((note) => note.note_id !== noteId);
+            writeToFile('./db/db.json', dbAfterDelete);
+    
+            res.json(`Note with the ID '${noteId}' has been deleted.`);     // may need to change this response
+    });
+});
 
 // POST route for adding a new note
 router.post('/', (req, res) => {
@@ -25,7 +36,7 @@ router.post('/', (req, res) => {
   
         // Call helper fs util function to modify 'db'
         readAndAppend(newNote, './db/db.json');
-        res.json(`Note added successfully`);        // may want a different response here
+        res.json(newNote);        // this response work?
     } else {
         res.error('Error in adding note');
     }
